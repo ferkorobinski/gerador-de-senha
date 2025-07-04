3f986ce08ad4de359636a668f29fe11409b040b3
ferkorobinski <142402938+ferkorobinski@users.noreply.github.com>
2025-07-04 08:11:28 -0300

    ok

--- main.js
+++ main.js
@@ -75,9 +75,9 @@ function classificaSenha(tamanhoAlfabeto){
     }
       else if (tamanhoSenha > 5 && tamanhoSenha < 12 ){
           forcaSenha.classList.add('media');
-      else if (tamanhoSenha >= 5){
+      } else if (tamanhoSenha >= 5){
 
 
-    forcaSenha.classList.add('forte');
+    forcaSenha.classList.add('fraca');
 }
 }
