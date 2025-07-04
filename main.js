2b11f309ff8f7cb1e36d32b2f4c8aadc701a1ac0
ferkorobinski <142402938+ferkorobinski@users.noreply.github.com>
2025-07-04 07:46:07 -0300

    ok

--- main.js
+++ main.js
@@ -66,6 +66,7 @@ function geraSenha() {
 }
 
 function classificaSenha(){
+    let entropia = tamanhoSenha * Math.log2(alfabeto.length);
     forcaSenha.classList.remove('fraca','media','forte');
 
     if (tamanhoSenha > 11){
